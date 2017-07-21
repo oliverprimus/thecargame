0849b2ca50debb67c8d51847481f0b6981142b9a
“Oliver <oliver@thefourohfive.com>
2017-07-21 18:09:55 +0100

    Javascript

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
-function myFunction() {
-    document.getElementById("score").innerHTML = "1";
+var total = 0;
+var bonus;
+
+function addToTotal(score) {
+  total += score;
+  document.getElementById("idiot").innerHTML = total;
+
+    if (bonus) {
+        total += score;
+        bonus = false;
+        document.getElementById("idiot").innerHTML = total;
 }
+    }
