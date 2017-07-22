11a2c6163623343d74280f6d79a12a8684dc0be0
“Oliver <oliver@thefourohfive.com>
2017-07-22 12:44:35 +0100

    Latest Update.

--- game.js
+++ game.js
@@ -1,13 +1,26 @@
 var total = 0;
 var bonus;
+var lastValue = [];
 
 function addToTotal(score) {
-  total += score;
-  document.getElementById("idiot").innerHTML = total;
-
     if (bonus) {
-        total += score;
+        score = score * 2;
         bonus = false;
         document.getElementById("idiot").innerHTML = total;
 }
+total += score;
+lastValue.push(score);
+document.getElementById("idiot").innerHTML = total;
     }
+
+function myReset(reset){
+bonus = false;
+total = 0;
+document.getElementById("idiot").innerHTML = total;
+}
+
+function undoLastAdd(){
+total -= lastValue[lastValue.length-1];
+lastValue.splice(-1,1);
+document.getElementById("idiot").innerHTML = total;
+}
